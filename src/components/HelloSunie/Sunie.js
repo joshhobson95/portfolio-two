a8c0e24917d6382c23374c6c429cdef40d6231f6
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-05 13:03:27 -0700

    wording

--- src/components/HelloSunie/Sunie.js
+++ src/components/HelloSunie/Sunie.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Sunie.css'
 import bigicons from '../../assets/HelloSunie/bigicons.png'
-import emailstuff from '../../assets/HelloSunie/emailstuff.png'
 import eventdetails from '../../assets/HelloSunie/EventDetials.png'
 import mainpage from '../../assets/HelloSunie/mainpage.png'
 import metabox from '../../assets/HelloSunie/metabox.png'
 import sunielogo from '../../assets/HelloSunie/hello-sunie.png'
 import jhlogo from '../../assets/Other/newJHlogo.png'
+import acf from '../../assets/HelloSunie/acf.png'
+import customplugin from '../../assets/HelloSunie/customplugin.png'
+import details from '../../assets/HelloSunie/details.png'
+import graphs from '../../assets/HelloSunie/graphs.png'
+
 
 function Sunie() {
   return (
@@ -20,13 +24,13 @@ function Sunie() {
 
           <h1>Hello Sunie Software Development</h1>
 
-          <p>A team of developers welcomed me in to help create this SaaS product called Hello Sunie. This product is used by New Mexico utility companies and contractors to manage schedules, billings, invoices, clientele and more for the aforementioned users. During my time with the team, we worked together to solve complex problems and write custom solutions to give Hello Sunie a competitive edge and a unique draw.</p>
+          <p>A team of developers welcomed me in to help create this SaaS product called Hello Sunie. This product's intended use is New Mexico utility companies and contractors to manage schedules, billings, invoices, clientele and more. During my time with the team, we worked together to solve complex problems and write custom solutions to give Hello Sunie a competitive edge and a unique draw.</p>
         </div>
 
         
     <div className='sunie_button_container'>
         <div className='sunie_button_backdrop'></div>
-    <button>Visit Website</button>
+    <button>Visit CodeSandbox</button>
     </div>
 
 
@@ -69,6 +73,7 @@ function Sunie() {
 
           <div className='key_grid_one'>
             <h2>HTML Buildout</h2>
+            <p>In the execution of the HelloSunie project, I meticulously crafted the HTML code, seamlessly integrating responsive design elements and optimizing the structure to enhance user experience, demonstrating proficiency in web development and attention to detail</p>
           <div className='key_grid_one_inner'>
               <div className='inner_square'>
               <img className='mainpage' src={mainpage} alt=''/>
@@ -81,10 +86,12 @@ function Sunie() {
       </div>
 
                     <div className='key_grid_two'>
-                      <h2>Custom Plugin Solutions</h2>
+                      <h2>MetaBox and ACF</h2>
+                      <p>Next, I elevated Metabox customization by implementing dynamic icons that change appearance based on received data, enhancing both the visual appeal and functionality of our site. Additionally, I spearheaded the creation of comprehensive custom post types and a robust data structure using Advanced Custom Fields (ACF), contributing significantly to HelloSunie's efforts in building a stellar product</p>
           <div className='key_grid_two_inner'>
-
-          <img className='emailstuff' src={emailstuff} alt=''/>
+          <div className='acf_container'>
+          <img className='acf' src={acf} alt=''/>
+          </div>
           <div className='inner_square2'>
           <img className='metabox' src={metabox} alt=''/>
           <img className='eventdetails' src={eventdetails} alt=''/>
@@ -112,19 +119,20 @@ function Sunie() {
 
 <div className='splp_2'>
 <div className='splp_inner_1'>
-  <h3>Advanced Custom Fields</h3>
+  <h3>Custom Plugin Solutions</h3>
   <p>
-  In developing the website for Hello Sunie, I strategically employed Advanced Custom Fields to effectively structure and organize post types and user data. This customization allowed for a seamless and tailored user experience, ensuring that information is efficiently categorized and easily accessible throughout the entire site. By leveraging the capabilities of Advanced Custom Fields, I aimed to enhance the overall functionality and user interface, specifically catering to the unique needs of the utility management industry.
+  In the development of a custom WordPress plugin for the project, I innovatively designed and implemented an intuitive admin calendar feature. This dynamic tool not only allowed administrative users to schedule services, complete with the ability to remove items seamlessly, but also facilitated personalized email notifications, delivering carefully formatted tables containing selected data to recipients as per the client's unique preferences
   </p>
-  <img src={jhlogo} />
+  <img className='customplugin' src={customplugin} />
 </div>
 
 <div className='splp_inner_2'>
-  <h3>Graphing Data</h3>
+  <h3>Handling and Graphing Data</h3>
   <p>
-  As well as HTML builds out and icongraphy added to Metaboxes, I jazzed up the site by adding custom graphing functions. Now, we've got visually appealing graphs with icons, mapping multiple sets of data all over the place. It's not just about numbers; it's about making data fun and easy to understand across the entire site.
+  In overseeing the data direction for specific service and project views, I meticulously curated detailed pages that dynamically showcased information relevant to the selected service. Elevating the user experience, I crafted custom PHP functions to generate insightful graphs from scratch, seamlessly integrating them into the HTML and CSS structure, thereby delivering visually compelling representations of pertinent data tailored to each viewed service or project
   </p>
-  <img src={jhlogo} />
+  <img className='details' src={details} />
+  <img className='graphs' src={graphs} />
 </div>
 </div>
 
