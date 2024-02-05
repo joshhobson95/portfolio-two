9555296df574b74c1b4e1b3989d38092c4030e34
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-05 16:53:07 -0700

    project page pictures

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,10 @@ import ParticleBackdrop from './Background/ParticleBackdrop'
 import './Projects.css' 
 import cashHouse from '../assets/Other/cashHouse.jpg'
 import sunie from '../assets/Other/Sunie-stock.jpg'
+import chicago from '../assets/Other/chitown.jpg'
+import santafe from '../assets/Other/Santafe.jpg'
+import pilytix from '../assets/Other/pilytix.jpg'
+import pokemon from '../assets/Other/Pokemon.jpg'
 import { NavLink } from 'react-router-dom'
 
 function Projects() {
@@ -16,6 +20,27 @@ function Projects() {
 
         <div className='grid_outer'>
 
+
+        <div className='grid_item_projects'>
+            <div className='cashHouse_text'>
+                            <h1>Center for Relational Learning Web Development</h1>         
+                    </div>
+            <NavLink to='/hellosunie'>
+                <img alt='' src={santafe} className='sunie'/>
+               </NavLink>
+
+            </div>
+
+
+            <div className='grid_item_projects'>
+            <div className='cashHouse_text'>
+                            <h1>Hello Sunie Software Development</h1>         
+                    </div>
+            <NavLink to='/hellosunie'>
+                <img alt='' src={sunie} className='sunie'/>
+               </NavLink>
+            </div>
+
             <div className='grid_item_projects'>
                     <div className='cashHouse_text'>
                             <h1>Jericho Nursery Website Revitalization</h1>         
@@ -28,17 +53,30 @@ function Projects() {
 
             <div className='grid_item_projects'>
             <div className='cashHouse_text'>
-                            <h1>Hello Sunie Software Development</h1>         
+                            <h1>Collaborative Counselling Design Work</h1>         
                     </div>
             <NavLink to='/hellosunie'>
-                <img alt='' src={sunie} className='sunie'/>
+                <img alt='' src={chicago} className='sunie'/>
                </NavLink>
+
             </div>
+
             <div className='grid_item_projects'>
-  
+            <div className='cashHouse_text'>
+                            <h1>Pilytix Assesment </h1>         
+                    </div>
+            <NavLink to='/hellosunie'>
+                <img alt='' src={pilytix} className='sunie'/>
+               </NavLink>
 
             </div>
             <div className='grid_item_projects'>
+            <div className='cashHouse_text'>
+                            <h1>Shiny Pokemon Checklist</h1>         
+                    </div>
+            <NavLink to='/hellosunie'>
+                <img alt='' src={pokemon} className='sunie'/>
+               </NavLink>
 
             </div>
 
