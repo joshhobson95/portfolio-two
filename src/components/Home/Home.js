226a81bc6909582aa7deb4e3ee957212c52f4cae
Josh Hobson <joshuahobson1995@gmail.com>
2024-04-24 11:16:10 -0600

    modal open on initial visit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,81 +1,55 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import ParticleBackdrop from '../Background/ParticleBackdrop';
 import './Home.css'
 import Modal from './Modal';
 import newlogo from '../../assets/Other/newJHlogo.png'
 import { NavLink } from 'react-router-dom';
 
-
 function Home() {
-
     const modalRef = useRef(null);
+    const [isModalOpen, setIsModalOpen] = useState(true);
 
     const toggleModalVisibility = () => {
-      const whiteSquareElement = document.querySelector('.white_square');
-  
-      if (whiteSquareElement.classList.contains('modal-open')) {
-        whiteSquareElement.classList.remove('modal-open');
-      } else {
-        whiteSquareElement.classList.add('modal-open');
-      }
-      
-      const modalElement = modalRef.current;
-      if (modalElement) {
-        modalElement.style.display =
-          modalElement.style.display === 'none' ? 'block' : 'none';
-      }
+        setIsModalOpen(!isModalOpen);
     };
-  
 
-
-
-
-  return (
-    <div className='home'>
-
-
-<div className="header_shell">
-        <div className="header_left">
-            <div className="inner_left">
-            <h1 className="JoshHobson">Josh Hobson</h1>
-            <h1>Web Developer</h1>
+    return (
+        <div className='home'>
+            <div className="header_shell">
+                <div className="header_left">
+                    <div className="inner_left">
+                        <h1 className="JoshHobson">Josh Hobson</h1>
+                        <h1>Web Developer</h1>
+                    </div>
+                </div>
             </div>
+
+            <div className={`white_square ${isModalOpen ? 'modal-open' : ''}`} onClick={toggleModalVisibility}>
+                <p className='x'>x</p>
+            </div>
+            <div className="white_square2"></div>
+            <div className="white_square3"></div>
+
+            {isModalOpen &&
+                <div ref={modalRef}>
+                    <br></br>
+                    <Modal />
+                </div>
+            }
+
+            <div className="newlogo_container">
+                <img className="newlogohome" src={newlogo} alt=""/>
+            </div>
+
+            <div className='secret'>
+                <NavLink to='/secret'>
+                    <button className='themoon'>The moon</button>
+                </NavLink>
+            </div>
+
+            <ParticleBackdrop />
         </div>
-
-</div>
-
-<div className="white_square" onClick={toggleModalVisibility}>
-  <p className='x'>x</p>
-</div>
-<div className="white_square2">
-</div>
-<div className="white_square3">
-</div>
-
-
-<div ref={modalRef} style={{ display: 'none' }}>
-  <br></br>
-        <Modal />
-      </div>
-
-<div className="newlogo_container">
-  <img className="newlogohome" src={newlogo} alt=""/>
-</div>
-
-
-
-
-<div className='secret'>
-<NavLink to='/secret'>
-<button className='themoon'>The moon</button>
-</NavLink>
-</div>
-
-<ParticleBackdrop />
-
-    
-    </div>
-  )
+    )
 }
 
-export default Home
+export default Home;
